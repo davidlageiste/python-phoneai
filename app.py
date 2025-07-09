ff7006d38405d50bc3784bd6ff6c3da9cd8d19e8
LAGEISTE David <lageiste.david@gmail.com>
2025-07-09 12:02:44 +0200

    urgence request

--- app.py
+++ app.py
@@ -2227,7 +2227,7 @@ async def rdv_exam_type():
         exam_type = await task_type
         task_urgence = asyncio.create_task(get_urgence_async(user_response, exam_type["type_examen_id"]))
         urgence = await task_urgence
-        if urgence is True:
+        if urgence is "True":
             hang_up(
                 "Il semblerait que vous appeliez pour une urgence. Je vous transfère vers une secrétaire.",
                 caller,
@@ -2677,7 +2677,7 @@ async def handleResponse():
             exam_type = await task_type
             task_urgence = asyncio.create_task(get_urgence_async(user_response, exam_type["type_examen_id"]))
             urgence = await task_urgence
-            if urgence is True:
+            if urgence is "True":
                 hang_up(
                     "Il semblerait que vous appeliez pour une urgence. Je vous transfère vers une secrétaire.",
                     caller,
@@ -2797,7 +2797,7 @@ async def handleResponse():
         # user_response = request.json[0].get("data").get("speechResult").get("speech")
         task_urgence = asyncio.create_task(get_urgence_async(user_response, calls[caller].rdv["exam_id"]))
         urgence = await task_urgence
-        if urgence is True:
+        if urgence is "True":
             hang_up(
                 "Il semblerait que vous appeliez pour une urgence. Je vous transfère vers une secrétaire.",
                 caller,
