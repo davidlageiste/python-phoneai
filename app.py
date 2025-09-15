c952a4259c6a71545f5c37623fde141068f4d045
0xenj <enzo.jakobasch@gmail.com>
2025-09-15 15:21:34 +0200

    app_url

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ SPEECH_REGION = "eastus"
 #     "mongodb+srv://neuracorp:amaCtNnLIHMJ4NGZ@riva.yiylf96.mongodb.net/neuracorp"
 # )
 MONGO_URL = "mongodb+srv://lageistedavid:eaZOnmgtcNN1oGxU@cluster0.pjma4cx.mongodb.net/neuracorp"
-APP_URL = "talkpreprodapi.azurewebsites.net"
+APP_URL = "lyrae-demo.azurewebsites.net"
 API_URL = "sparkso-universite.com:8080"
 
 
